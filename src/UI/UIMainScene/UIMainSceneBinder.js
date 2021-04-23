abdc1ab0d3b2bf67e41136f599e2e7c2b428690d
dongtech <zld126126@126.com>
2021-04-23 19:49:37 +0800

    minor

--- src/UI/UIMainScene/UIMainSceneBinder.js
+++
@@ -1,8 +0,0 @@
-/** This is an automatically generated class by FairyGUI. Please do not modify it. **/
-import UIMainScene from "./UIMainScene";
-export default class UIMainSceneBinder {
-    static bindAll() {
-        fgui.UIObjectFactory.setExtension(UIMainScene.URL, UIMainScene);
-    }
-}
-//# sourceMappingURL=UIMainSceneBinder.js.map
